5b5faa06fc930103595d6cfb50a077c42d0a636f
Julian <julianser@hotmail.com.ar>
2023-03-29 15:25:14 -0300

    Se realizo la logica de adivinar numero y se agrego una imagen

--- js/numerosMagicos.js
+++ js/numerosMagicos.js
@@ -1,14 +1,30 @@
 let numeroMagico = 0;
 let formulario = document.getElementById('formu');
-formulario.addEventListener('submit',adivinar)
+formulario.addEventListener('submit',adivinar);
 
 function numeroRandom(){
-    numeroMagico = Math.ceil(Math.random()* (100 - 1));
+    numeroMagico = Math.floor(Math.random()* 100) + 1;
     console.log(numeroMagico);
     return numeroMagico;
 }
 
-function adivinar(){
+function adivinar(e){
     e.preventDefault();
-    console.log("desde adivinar")
+    console.log("desde adivinar");
+
+    let input = document.querySelector('input');
+    console.log(input);
+    let dato = parseInt(input.value);
+    
+    if(dato === numeroMagico ){
+        alert("Adivinaste el numero, Ganaste!!!")
+    }
+    else if(dato > numeroMagico)
+    {
+        alert("El numero que ingresaste es mayor que el numero magico")
+    }
+    else if(dato < numeroMagico)
+    {
+        alert("El numero que ingresaste es menor que el numero magico")
+    }
 }
