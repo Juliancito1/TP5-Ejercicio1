84af055af0bb9b679062cbe4fdad2658a240ad2e
Julian <julianser@hotmail.com.ar>
2023-03-29 19:38:53 -0300

    Se agregaron validaciones de que sea numero lo que se ingresa y que el usuario ingrese numeros del 1 al 100

--- js/numerosMagicos.js
+++ js/numerosMagicos.js
@@ -1,4 +1,5 @@
 let numeroMagico = 0;
+let numeroGenerado = false; 
 let formulario = document.getElementById('formu');
 formulario.addEventListener('submit',adivinar);
 
@@ -6,17 +7,22 @@ function numeroRandom(){
     numeroMagico = Math.floor(Math.random()* 100) + 1;
     console.log(numeroMagico);
     alert("El numero fue generado, comienza a adivinar");
+    numeroGenerado = true;
     return numeroMagico;
 }
+
 
 function adivinar(e){
     e.preventDefault();
     console.log("desde adivinar");
 
+    if(numeroGenerado){
     let input = document.querySelector('input');
     console.log(input);
     let dato = parseInt(input.value);
     
+    if(dato && !isNaN(dato) && dato >=1 && dato <=100)
+    {
     if(dato === numeroMagico ){
         alert("Adivinaste el numero, Ganaste!!!")
     }
@@ -27,5 +33,11 @@ function adivinar(e){
     else if(dato < numeroMagico)
     {
         alert("El numero que ingresaste es menor que el numero magico")
+    }
+    }else{
+        alert("No ingresaste un numero o ingresaste un numero erróneo")
+    }
+    }else{
+        alert("El numero magico no ha sido generado, haz click en comenzar el juego")
     }
 }
